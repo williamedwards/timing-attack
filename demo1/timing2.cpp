cec8bed99499fcc63eae09db575c784f54e1dc43
William Edwards <williamedwards314@gmail.com>
2019-03-28 01:46:23 -0400

    Added data trimming.

--- demo1/timing2.cpp
+++ demo1/timing2.cpp
@@ -3,13 +3,23 @@
 #include <chrono>
 #include <vector>
 #include <cmath>
+#include <algorithm>
 
 
 #include "secret.h"
 
 constexpr int iters = 1000;
+constexpr int warm_up = 500;
 constexpr double zcrit = 2.58;
+constexpr double trim_percentage = 0.1;
 namespace chrono = std::chrono;
+
+void trim(std::vector<int> & vals) {
+    std::sort(vals.begin(), vals.end());
+    int num_drop = vals.size() * trim_percentage;
+    vals.erase(vals.begin(), vals.begin() + num_drop);
+    vals.erase(vals.end() - num_drop, vals.end());
+}
 
 double mean(const std::vector<int> & vals) {
     double sum = 0.0;
@@ -22,7 +32,7 @@ double stdev(const std::vector<int> & vals) {
     double sample_mean = mean(vals);
     double sum_squared_diff = 0.0;
     for (int val : vals)
-        sum_squared_diff = (val - sample_mean) * (val - sample_mean);
+        sum_squared_diff += (val - sample_mean) * (val - sample_mean);
     return sqrt(sum_squared_diff / (vals.size() - 1));
 }
 
@@ -36,8 +46,8 @@ std::pair<double, double> ci(const std::vector<int> & vals) {
 }
 
 int main() {
-    std::string pass_a = "aaaaaaaaaaaaaaaaaaa";
-    std::string pass_b = "eeeeeeeeeeeeeeeeeee";
+    std::string pass_a = "edvishwordforfriend";
+    std::string pass_b = "edvishwordforfriend";
     std::vector<int> times_a;
     std::vector<int> times_b;
     for (int i = 0; i < iters; i++) {
@@ -46,13 +56,23 @@ int main() {
         auto end = chrono::steady_clock::now();
         auto duration = chrono::duration_cast<chrono::nanoseconds>(end - start);
         times_a.push_back(duration.count());
+        std::cout << "A: " << duration.count() << std::endl;
 
-        start = chrono::steady_clock::now();
+        auto start_b = chrono::steady_clock::now();
         check_password(pass_b);
-        end = chrono::steady_clock::now();
-        duration = chrono::duration_cast<chrono::nanoseconds>(end - start);
-        times_b.push_back(duration.count());
+        auto end_b = chrono::steady_clock::now();
+        auto duration_b = chrono::duration_cast<chrono::nanoseconds>(end_b - start_b);
+        times_b.push_back(duration_b.count());
+        std::cout << "B: " << duration_b.count() << std::endl;
     }
+    // // Skip warm-up
+    // times_a.erase(times_a.begin(), times_a.begin() + warm_up);
+    // times_b.erase(times_b.begin(), times_b.begin() + warm_up);
+    // Trim the data
+    trim(times_a);
+    trim(times_b);
+
+    // Compute and print statistics
     std::cout << "Mean A: " << mean(times_a) << std::endl;
     std::cout << "StDev A: " << stdev(times_a) << std::endl;
     auto ci_a = ci(times_a);
