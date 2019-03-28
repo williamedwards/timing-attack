43f759732ddd1d4c846535e659db975f173c5200
William Edwards <williamedwards314@gmail.com>
2019-03-28 03:22:58 -0400

    Working commit on timing3

--- demo1/timing3.cpp
+++ demo1/timing3.cpp
@@ -95,47 +95,16 @@ std::string crack_pair(std::string prefix) {
         }
         current_num++;
     }
-    std::string max_pair = get_pair_from_num(max_num)
+    std::string max_pair = get_pair_from_num(max_num);
     std::cout << "Max pair is: " << max_pair << std::endl;
     
     return max_pair;
 }
 
 int main() {
-    std::string pass_a = "el-----------------";
-    std::string pass_b = "-------------------";
-
-    // Initialize random stuff
-    std::random_device rd;
-    std::default_random_engine random(rd());
-    std::uniform_int_distribution<int> dist(0, num_bound - 1);
-
-    std::vector<std::vector<int> > times;
-    for (int i = 0; i < num_bound; i++)
-        times.emplace_back();
-
-    for (int i = 0; i < iters; i++) {
-        int choice = dist(random);
-        std::string pass = get_pair_from_num(choice);
-        pass += "-----------------";
-            
-        auto start = chrono::steady_clock::now();
-        check_password(pass);
-        auto end = chrono::steady_clock::now();
-        auto duration = chrono::duration_cast<chrono::nanoseconds>(end - start);
-        times.at(choice).push_back(duration.count());
+    std::string prefix = "";
+    for (int i = 0; i < password_length; i += 2) {
+        prefix += crack_pair(prefix); 
     }
-
-    double max_mean = -1.0;
-    int max_num = -1;
-    int current_num = 0;
-    for (const auto& time_list : times) {
-        double current_mean = mean(time_list);
-        if (current_mean > max_mean) {
-            max_mean = current_mean;
-            max_num = current_num;
-        }
-        current_num++;
-    }
-    std::cout << "Max pair is: " << get_pair_from_num(max_num) << std::endl;
+    std::cout << "Final password: " << prefix << std::endl;
 }
