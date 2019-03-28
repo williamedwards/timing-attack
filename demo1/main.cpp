19ad6ae6ebd6c38871d0138ccf91713ae6bb5474
William Edwards <williamedwards314@gmail.com>
2019-03-27 23:25:26 -0400

    More stuff.

--- demo1/main.cpp
+++ demo1/main.cpp
@@ -1,16 +1,17 @@
-#include<iostream>
+#include <iostream>
+#include <chrono>
+#include <thread>
 
 constexpr char secret_password[] = "elvishwordforfriend";
 
 int main(int argc, char ** argv) {
-    if (argc != 2) {
-        std::cout << "Please enter the password" << std::endl;
-        return 1;
-    }
-    std::string password = argv[1];
+    std::string password_input;
 
-    if (password == secret_password)
-        std::cout << "Access Granted" << std::endl;
-    else
-        std::cout << "Access Denied" << std::endl;
+    while (std::getline(std::cin, password_input)) {
+        if (password_input == secret_password)
+            std::cout << "Access Granted" << std::endl;
+        else
+            std::cout << "Access Denied" << std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
 }
--- demo1/timer.py
+++ demo1/timer.py
@@ -4,26 +4,29 @@ import time
 import os
 import random
 
-iterations = 1000
-command_a = ["./main", "alvishwordforfriend"]
-command_b = ["./main", "elvishwordforfrienb"]
+iterations = 100
+command = ["./main"]
+pass_a = b"alvishwordforfriend"
+pass_b = b"elvishwordforfrienb"
 
 def main():
     FNULL = open(os.devnull, "w")
     times_a = []
     times_b = []
-    for _ in range(iterations):
-        if random.random() < 0.5:
-            # Do command a
+    with subprocess.Popen(command, stdout=subprocess.PIPE, stdin=subprocess.PIPE) as p:
+        for _ in range(iterations):
+            if random.random() < 0.5:
+                password = pass_a
+                times = times_a
+            else:
+                password = pass_b
+                times = times_b
             start_time = time.time()
-            subprocess.call(command_a, stdout=FNULL)
+            p.stdin.write(pass_a + b"\n")
+            p.stdin.flush()
+            response = p.stdout.readline()
             elapsed = time.time() - start_time
-            times_a.append(elapsed)
-            # Do command b
-            start_time = time.time()
-            subprocess.call(command_b, stdout=FNULL)
-            elapsed = time.time() - start_time
-            times_b.append(elapsed)
+            times.append(elapsed)
     mean_a = sum(times_a) / len(times_a)
     mean_b = sum(times_b) / len(times_b)
     print(mean_b - mean_a)
